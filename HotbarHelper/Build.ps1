cccba4ba7aea255dc5beee5f3607a3c735f6ceec
Danny <225295380+Fre-yin@users.noreply.github.com>
2026-09-11 11:34:16 +0200

    Include LocalOnlyChecks in helper test build

--- HotbarHelper/Build.ps1
+++ HotbarHelper/Build.ps1
@@ -24,7 +24,7 @@ if ((Get-FileHash -LiteralPath $taskBepPackage -Algorithm SHA256).Hash -ne '7719
 $taskResources = @(('/resource:' + $taskPackage + ',HotbarPackage.zip'), ('/resource:' + $taskBepPackage + ',BepInExPackage.zip'))
 $taskRefs = @('mscorlib','System','System.Core','System.Drawing','System.Windows.Forms','System.Web.Extensions','System.IO.Compression','System.IO.Compression.FileSystem','System.Xml') | ForEach-Object { '/reference:' + (Join-Path $taskFramework ($_ + '.dll')) }
 $taskCommon = @('/nologo','/noconfig','/nostdlib+','/langversion:latest','/warnaserror+','/optimize+','/platform:x64') + $taskRefs
-$taskCore = @('FrameworkTarget.cs','LosslessJson.cs','Profiles.cs','HotbarKeyProfile.cs','ReleaseInfo.cs','ModLoaders.cs','Operations.cs','LoadStatus.cs','UiText.cs','Updates.cs','UpdateSignature.cs','LocalMods.cs','LocalHelpers.cs','GameDiscovery.cs') | ForEach-Object { Join-Path $PSScriptRoot $_ }
+$taskCore = @('FrameworkTarget.cs', ... 'LocalHelpers.cs','LocalOnlyChecks.cs','GameDiscovery.cs')
 $taskLanguages = @('en','ko','fr','de','ru','zh-Hans','zh-Hant','ja','es','pt-BR') | ForEach-Object { '/resource:' + (Join-Path $PSScriptRoot ('Languages\' + $_ + '.json')) + ',Language.' + $_ + '.json' }
 $taskTrustPath = Join-Path $PSScriptRoot 'UpdateTrust.xml'
 if (-not (Test-Path -LiteralPath $taskTrustPath)) { throw 'Pinned public update key missing. Initialize publisher signing first.' }
